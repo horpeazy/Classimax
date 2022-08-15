0ac65000101ba47797f30a7aecf190fd6e9829a2
horpeazy <iyamuhope.nosa647@gmail.com>
2022-08-15 12:51:47 +0800

    feat: added transition for dropdown

--- src/Navbar.js
+++ src/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
         }
     }
 
-    const showSubLink = (e) => {
+    const showSubLink = () => {
         if (window.innerWidth >= 990){
             setShowSubmenu(true);
             const subLink = document.querySelector('.sub-link');
